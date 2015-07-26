e76084323495169d93a4aa6b7693a92e533d6610
Corbin Simpson <cds@corbinsimpson.com>
2015-07-25 18:29:33 -0700

    autohelp: AutoHelp can accomodate @methods. AutoHelp is fully armed.
    AutoHelp is here to help. Do not refuse AutoHelp.

--- typhon/autohelp.py
+++ typhon/autohelp.py
@@ -4,7 +4,7 @@
 
 import inspect
 
-from typhon.atoms import Atom
+from typhon.atoms import Atom, getAtom
 
 def autohelp(cls):
     """
@@ -12,20 +12,35 @@ def autohelp(cls):
 
     Do not mock AutoHelp. AutoHelp should not be engaged manually. AutoHelp is
     here to help.
+
+    AutoHelp can recover docstrings from @method-decorated methods. The
+    recovery process is automatic and, in most cases, painless. AutoHelp will
+    not administer any anesthetic. AutoHelp is here to help.
     """
 
     recv = cls.recv.__func__
     names = recv.__code__.co_names
 
+    # Retrieve atoms defined from module scope.
     module = inspect.getmodule(cls)
     availableAtoms = inspect.getmembers(module,
                                         lambda obj: isinstance(obj, Atom))
     availableAtoms = dict(availableAtoms)
 
+    # Discover atoms from recv().
     atoms = {}
     for name in names:
         if name in availableAtoms:
             atoms[availableAtoms[name]] = None
+
+    # Synthesize atoms from @methods.
+    methods = inspect.getmembers(cls,
+                                 lambda attr: hasattr(attr, "_monteMethod_"))
+    for name, method in methods:
+        argumentTypes, resultTypes = method._monteMethod_
+        atom = getAtom(name, len(argumentTypes))
+        doc = method.__doc__.decode("utf-8") if method.__doc__ else None
+        atoms[atom] = doc
 
     def respondingAtoms(self):
         return atoms
