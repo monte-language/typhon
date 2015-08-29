4f4db658571fd94cc21bf8946c871c0e2a5e05fd
Allen Short <ashort@mozilla.com>
2015-08-29 15:51:25 -0500

    Travis builds using Nix

---
+++ travis-build.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+set -e
+
+export NIX_CURL_FLAGS=-sS
+
+echo "=== Installing Nix..."
+# Install Nix
+bash <(curl -sS https://nixos.org/nix/install)
+source $HOME/.nix-profile/etc/profile.d/nix.sh
+
+# Make sure we can use hydra's binary cache
+sudo mkdir /etc/nix
+sudo tee /etc/nix/nix.conf <<EOF >/dev/null
+binary-caches = http://cache.nixos.org http://hydra.nixos.org
+trusted-binary-caches = http://hydra.nixos.org
+build-max-jobs = 4
+EOF
+
+source $HOME/.nix-profile/etc/profile.d/nix.sh
+nix-build default.nix -A mast
