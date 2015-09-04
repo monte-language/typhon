6719677700feb44ca5ac49ab652a563ba1daaeb6
Allen Short <ashort@mozilla.com>
2015-09-03 22:40:18 -0500

    ;

--- default.nix
+++ default.nix
@@ -1,6 +1,6 @@
 { system ? builtins.currentSystem }:
 let
-  nixpkgs = import <nixpkgs> { inherit system };
+  nixpkgs = import <nixpkgs> { inherit system; };
   jobs = with nixpkgs; rec {
     typhonVm_O2 = callPackage ./nix/vm.nix { buildJIT = false; };
     typhonVm = callPackage ./nix/vm.nix { buildJIT = true; };
