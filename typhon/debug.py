03b03761f3fb5f221fa6c07211a2fcd7b23383d3
Corbin Simpson <cds@corbinsimpson.com>
2015-11-06 14:37:56 -0800

    debug: Useful feedback.

--- typhon/debug.py
+++ typhon/debug.py
@@ -1,5 +1,5 @@
 from rpython.rlib.debug import debug_print
-from rpython.rlib.jit import JitHookInterface
+from rpython.rlib.jit import Counters, JitHookInterface
 
 class DebugPrinter(object):
 
@@ -24,13 +24,17 @@ class TyphonJitHooks(JitHookInterface):
 
     def on_abort(self, reason, jitdriver, greenkey, greenkey_repr, logops,
                  operations):
-        debugPrint("Aborted trace:", greenkey_repr, reason, "operations",
-                   str(len(operations)))
+        if True:
+            return
+        reasonString = Counters.counter_names[reason]
+        print "Aborted trace:", greenkey_repr, reasonString, "operations", len(operations)
 
     def after_compile(self, debug_info):
-        debugPrint("Compiled:", debug_info.get_greenkey_repr(), "operations",
-                   str(len(debug_info.operations)))
+        if True:
+            return
+        print "Compiled:", debug_info.get_greenkey_repr(), "operations", len(debug_info.operations)
 
     def after_compile_bridge(self, debug_info):
-        debugPrint("Compiled bridge: operations",
-                   str(len(debug_info.operations)))
+        if True:
+            return
+        print "Compiled bridge: operations", len(debug_info.operations)
