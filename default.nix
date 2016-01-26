1ddfc434c32b645fffd3105ceea54183cd1d3d1d
Corbin Simpson <cds@corbinsimpson.com>
2016-01-25 23:00:42 -0800

    nix: Start a derivation wrapper.

--- default.nix
+++ default.nix
@@ -1,11 +1,17 @@
 { system ? builtins.currentSystem }:
 let
   nixpkgs = import <nixpkgs> { inherit system; };
-  jobs = with nixpkgs; rec {
+  typhon = with nixpkgs; rec {
     typhonVm_O2 = callPackage ./nix/vm.nix { buildJIT = false; };
     typhonVm = callPackage ./nix/vm.nix { buildJIT = true; };
     mast = callPackage ./nix/mast.nix { typhonVm = typhonVm; };
     bench = callPackage ./nix/bench.nix { typhonVm = typhonVm; mast = mast; };
+
+    montePackage = s @ { name, version, ... }: let stuff = {
+      name = "monte-${name}-${version}";
+      buildInputs = [ typhonVm mast ];
+      doCheck = false;
+    }; in nixpkgs.stdenv.mkDerivation (s // stuff);
   };
 in
-  jobs
+  typhon
