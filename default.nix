8b3fe61585ee4527b816f5cdd3b7c337cd94aa32
Corbin Simpson <cds@corbinsimpson.com>
2016-01-26 19:16:36 -0800

    Hard.

--- default.nix
+++ default.nix
@@ -1,17 +1,31 @@
 { system ? builtins.currentSystem }:
 let
   nixpkgs = import <nixpkgs> { inherit system; };
+  lib = nixpkgs.lib;
   typhon = with nixpkgs; rec {
     typhonVm_O2 = callPackage ./nix/vm.nix { buildJIT = false; };
     typhonVm = callPackage ./nix/vm.nix { buildJIT = true; };
     mast = callPackage ./nix/mast.nix { typhonVm = typhonVm; };
     bench = callPackage ./nix/bench.nix { typhonVm = typhonVm; mast = mast; };
 
-    montePackage = s @ { name, version, ... }: let stuff = {
-      name = "monte-${name}-${version}";
-      buildInputs = [ typhonVm mast ];
-      doCheck = false;
-    }; in nixpkgs.stdenv.mkDerivation (s // stuff);
+    montePackage = s @ { name, version, src, ... }: let
+      mtSources = builtins.attrNames (builtins.readDir src);
+      mastNames = map (builtins.replaceStrings [".mt"] [".mast"]) mtSources;
+      montec = source: dest:
+        "${typhonVm}/mt-typhon -l ${mast}/mast ${mast}/mast/montec -mix -format mast $src/${source} ${dest}";
+      mastInstall = mast: "cp ${mast} $out/mast/";
+      stuff = {
+        name = "monte-${name}-${version}";
+        buildInputs = [ typhonVm mast ];
+        doCheck = false;
+        buildPhase = lib.concatStringsSep "\n"
+          (lib.zipListsWith montec mtSources mastNames);
+        installPhase = ''
+          mkdir -p $out/mast
+          '' + lib.concatStringsSep "\n" (map mastInstall mastNames);
+      };
+    in
+      nixpkgs.stdenv.mkDerivation (s // stuff);
   };
 in
   typhon
