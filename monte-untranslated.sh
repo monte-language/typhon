bf45c759fc5bac00cfd87be5e369b50c75bb543d
Allen Short <ashort@mozilla.com>
2016-01-31 14:59:57 -0600

    superseded by 'nix-shell -A typhonVm'

--- monte-untranslated.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env nix-shell
-#! nix-shell -i bash -p pypy libuv
-inputs=($nativeBuildInputs)
-libuv=${inputs[1]}
-TYPHON_INCLUDE_PATH=$libuv/include TYPHON_LIBRARY_PATH=$libuv/lib PYTHONPATH=pypy:. pypy main.py -l mast repl.ty
