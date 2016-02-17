ce230d6c12562f439b3ae1fe149ea6bb9dd3a5c8
Allen Short <ashort@mozilla.com>
2016-02-17 10:00:39 -0600

    fix missed pypy reference

--- nix/mt.nix
+++ nix/mt.nix
@@ -1,4 +1,4 @@
-{stdenv, lib, pypy, typhonVm, mast, nix, nix-prefetch-scripts}:
+{stdenv, lib, python27, typhonVm, mast, nix, nix-prefetch-scripts}:
 
 stdenv.mkDerivation {
     name = "mt";
@@ -13,7 +13,7 @@ stdenv.mkDerivation {
       cp nix/montePackage.nix $out/nix
       cat <(echo "FETCHERS = {'git': '${nix-prefetch-scripts + "/bin/nix-prefetch-git"}'}")  nix/mt-bake.py.in > $out/nix/mt-bake.py
       echo "${typhonVm}/mt-typhon -l ${mast}/mast -l ${mast} loader run repl" > $out/bin/mt-repl
-      echo "${pypy}/bin/pypy $out/nix/mt-bake.py; ${nix}/bin/nix-build -E \"let pkgs = import <nixpkgs> {}; in pkgs.callPackage $out/nix/montePackage.nix { typhonVm = ${typhonVm}; mast = ${mast}; } ./mt-lock.json \"" > $out/bin/mt-bake
+      echo "${python27}/bin/python $out/nix/mt-bake.py; ${nix}/bin/nix-build -E \"let pkgs = import <nixpkgs> {}; in pkgs.callPackage $out/nix/montePackage.nix { typhonVm = ${typhonVm}; mast = ${mast}; } ./mt-lock.json \"" > $out/bin/mt-bake
       chmod +x $out/bin/mt-repl
       chmod +x $out/bin/mt-bake
       '';
