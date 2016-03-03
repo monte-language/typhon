ad0c335e3fd94ec8c3208522d48122854a5e3c4c
Corbin Simpson <cds@corbinsimpson.com>
2016-03-03 13:54:21 -0800

    nix: Call a spade a spade; disable the JIT by default.

--- default.nix
+++ default.nix
@@ -3,8 +3,8 @@ let
   nixpkgs = import <nixpkgs> { inherit system; };
   lib = nixpkgs.lib;
   typhon = with nixpkgs; rec {
-    typhonVm_O2 = callPackage ./nix/vm.nix { buildJIT = false; };
-    typhonVm = callPackage ./nix/vm.nix { buildJIT = true; };
+    typhonVm = callPackage ./nix/vm.nix { buildJIT = false; };
+    typhonVmCrashy = callPackage ./nix/vm.nix { buildJIT = true; };
     mast = callPackage ./nix/mast.nix { typhonVm = typhonVm; };
     bench = callPackage ./nix/bench.nix { typhonVm = typhonVm; mast = mast; };
     mt = callPackage ./nix/mt.nix { typhonVm = typhonVm; mast = mast; };
