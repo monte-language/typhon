75888a9d55a007944f2687502e0c820fcd652b24
Corbin <cds@corbinsimpson.com>
2016-03-12 11:39:03 -0800

    nix/vm: Update to PyPy 5.0.0.
    This should not cause anybody to build a new PyPy. If it does, let me know.

--- nix/vm.nix
+++ nix/vm.nix
@@ -2,8 +2,8 @@
 
 # $ nix-prefetch-hg https://bitbucket.org/pypy/pypy
 let pypySrc = fetchzip {
-    url = "https://bitbucket.org/pypy/pypy/downloads/pypy-4.0.1-src.tar.bz2";
-    sha256 = "0saq5qd18wnqx9piyqcc3c23qbdnwbw8gxab5yyqpd0c44xymxw2";
+    url = "https://bitbucket.org/pypy/pypy/downloads/pypy-5.0.0-src.tar.bz2";
+    sha256 = "1hxjhm66081hnhdkf0fifswc9js9qi88zyy7pfpcwlxl029sddj0";
   };
   optLevel = if buildJIT then "-Ojit" else "-O2";
 in
