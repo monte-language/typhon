d74cd7112670fd5b25c7f50199529e9c501836a5
Allen Short <ashort@mozilla.com>
2016-06-01 17:55:40 -0500

    Remove old repl script, superseded by "monte repl".

--- nix-support/mast.nix
+++ nix-support/mast.nix
@@ -11,8 +11,6 @@ stdenv.mkDerivation {
       mkdir -p $out/bin
       cp -r mast loader.mast $out/
       cp -r mast repl.mast $out/
-      echo "${typhonVm}/mt-typhon -l $out/mast -l $out loader run repl" > $out/bin/monte
-      chmod +x $out/bin/monte
       '';
     checkPhase = "make testMast";
     doCheck = true;
