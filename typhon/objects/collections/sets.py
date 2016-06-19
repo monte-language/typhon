9e69c8b748a13b17edd0dd149df9dcf54aa212b2
Corbin <cds@corbinsimpson.com>
2016-06-18 18:46:24 -0700

    objects/collections/sets: ConstSet @methods.

--- typhon/objects/collections/sets.py
+++ typhon/objects/collections/sets.py
@@ -15,7 +15,7 @@
 from rpython.rlib.rarithmetic import intmask
 
 from typhon.atoms import getAtom
-from typhon.autohelp import autohelp
+from typhon.autohelp import autohelp, method
 from typhon.errors import Refused, WrongType, userError
 from typhon.objects.collections.helpers import monteSet
 from typhon.objects.constants import NullObject, wrapBool
@@ -92,7 +92,8 @@ class ConstSet(Object):
             x = intmask((1000003 * x) ^ y)
         return x
 
-    def printOn(self, printer):
+    @method("Void", "Any")
+    def _printOn(self, printer):
         printer.call(u"print", [StrObject(u"[")])
         for i, obj in enumerate(self.objectSet.keys()):
             printer.call(u"quote", [obj])
@@ -100,16 +101,26 @@ class ConstSet(Object):
                 printer.call(u"print", [StrObject(u", ")])
         printer.call(u"print", [StrObject(u"].asSet()")])
 
+    @method("Any")
     def _makeIterator(self):
+        """
+        Create an iterator for this collection's contents.
+        """
+
         from typhon.objects.collections.lists import listIterator
         return listIterator(self.objectSet.keys())
 
+    @method("Bool", "Any")
     def contains(self, needle):
+        """
+        Determine whether an element is in this collection.
+        """
+
         return needle in self.objectSet
 
+    @method("Set", "Set", _verb="and")
     @profileTyphon("Set.and/1")
-    def _and(self, otherSet):
-        other = unwrapSet(otherSet)
+    def _and(self, other):
         if (len(self.objectSet) > len(other)):
             bigger = self.objectSet
             smaller = other
@@ -121,131 +132,98 @@ class ConstSet(Object):
         for k in smaller:
             if k in bigger:
                 rv[k] = None
-        return ConstSet(rv)
+        return rv
 
+    @method("Set", "Set", _verb="or")
     @profileTyphon("Set.or/1")
     def _or(self, other):
         # XXX This is currently linear time. Can it be better? If not, prove
         # it, please.
         rv = self.objectSet.copy()
-        for ok in unwrapSet(other).keys():
+        for ok in other.keys():
             if ok not in rv:
                 rv[ok] = None
-        return ConstSet(rv)
+        return rv
 
+    # XXX Decide if we follow python-style '-' or E-style '&!' here.
+    @method.py("Set", "Set")
     @profileTyphon("Set.subtract/1")
-    def subtract(self, otherSet):
-        other = unwrapSet(otherSet)
+    def subtract(self, other):
         rv = self.objectSet.copy()
         for ok in other.keys():
             if ok in rv:
                 del rv[ok]
-        return ConstSet(rv)
+        return rv
 
-    def slice(self, start, stop=-1):
-        assert start >= 0
-        if stop < 0:
-            keys = self.objectSet.keys()[start:]
-        else:
-            keys = self.objectSet.keys()[start:stop]
+    @method("Set", "Set")
+    def butNot(self, other):
+        return self.subtract(other)
+
+    @method("Set", "Int")
+    def slice(self, start):
+        if start < 0:
+            raise userError(u"slice/2: Negative start")
+        keys = self.objectSet.keys()[start:]
         rv = monteSet()
         for k in keys:
             rv[k] = None
-        return ConstSet(rv)
+        return rv
 
+    @method("Set", "Int", "Int", _verb="slice")
+    def _slice(self, start, stop):
+        if start < 0:
+            raise userError(u"slice/2: Negative start")
+        if stop < 0:
+            raise userError(u"slice/2: Negative stop")
+        keys = self.objectSet.keys()[start:stop]
+        rv = monteSet()
+        for k in keys:
+            rv[k] = None
+        return rv
+
+    @method("Int")
     def size(self):
         return len(self.objectSet)
 
+    @method("Set")
     def snapshot(self):
-        return ConstSet(self.objectSet.copy())
+        return self.objectSet.copy()
 
-    def recv(self, atom, args):
+    @method("List")
+    def _uncall(self):
         from typhon.objects.collections.lists import wrapList
+        from typhon.objects.collections.maps import EMPTY_MAP
+        # [1,2,3].asSet() -> [[1,2,3], "asSet"]
+        rv = wrapList(self.objectSet.keys())
+        return [rv, StrObject(u"asSet"), wrapList([]), EMPTY_MAP]
 
-        # _makeIterator/0: Create an iterator for this collection's contents.
-        if atom is _MAKEITERATOR_0:
-            return self._makeIterator()
+    @method("Set")
+    def asSet(self):
+        return self.objectSet
 
-        if atom is _PRINTON_1:
-            printer = args[0]
-            self.printOn(printer)
-            return NullObject
+    @method("Any")
+    def diverge(self):
+        return FlexSet(self.objectSet.copy())
 
-        if atom is _UNCALL_0:
-            from typhon.objects.collections.maps import EMPTY_MAP
-            # [1,2,3].asSet() -> [[1,2,3], "asSet"]
-            rv = wrapList(self.objectSet.keys())
-            return wrapList([rv, StrObject(u"asSet"), wrapList([]),
-                              EMPTY_MAP])
+    @method("List")
+    def asList(self):
+        return self.objectSet.keys()
 
-        # contains/1: Determine whether an element is in this collection.
-        if atom is CONTAINS_1:
-            return wrapBool(self.contains(args[0]))
+    @method("Set", "Any", _verb="with")
+    def _with(self, key):
+        d = self.objectSet.copy()
+        d[key] = None
+        return d
 
-        # size/0: Get the number of elements in the collection.
-        if atom is SIZE_0:
-            return IntObject(self.size())
-
-        # slice/1 and slice/2: Select a subrange of this collection.
-        if atom is SLICE_1:
-            start = unwrapInt(args[0])
-            try:
-                return self.slice(start)
-            except IndexError:
-                raise userError(u"slice/1: Index out of bounds")
-
-        # slice/1 and slice/2: Select a subrange of this collection.
-        if atom is SLICE_2:
-            start = unwrapInt(args[0])
-            stop = unwrapInt(args[1])
-            try:
-                return self.slice(start, stop)
-            except IndexError:
-                raise userError(u"slice/1: Index out of bounds")
-
-        # snapshot/0: Create a new constant collection with a copy of the
-        # current collection's contents.
-        if atom is SNAPSHOT_0:
-            return self.snapshot()
-
-        if atom is ASSET_0:
-            return self
-
-        if atom is DIVERGE_0:
-            return FlexSet(self.objectSet)
-
-        if atom is AND_1:
-            return self._and(args[0])
-
-        # or/1: Unify the elements of this collection with another.
-        if atom is OR_1:
-            return self._or(args[0])
-
-        # XXX Decide if we follow python-style '-' or E-style '&!' here.
-        if atom is SUBTRACT_1:
-            return self.subtract(args[0])
-
-        if atom is ASLIST_0:
-            return wrapList(self.objectSet.keys())
-
-        if atom is BUTNOT_1:
-            return self.subtract(args[0])
-
-        if atom is WITH_1:
-            key = args[0]
+    @method("Set", "Any")
+    def without(self, key):
+        # If the key isn't in the map, don't bother copying.
+        if key in self.objectSet:
             d = self.objectSet.copy()
-            d[key] = None
-            return ConstSet(d)
-
-        if atom is WITHOUT_1:
-            key = args[0]
-            d = self.objectSet.copy()
-            # Ignore the case where the key wasn't in the map.
-            if key in d:
-                del d[key]
-            return ConstSet(d)
-
-        raise Refused(self, atom, args)
+            del d[key]
+            return d
+        else:
+            return self.objectSet
 
 
 @autohelp
