8786021154fcbf135751665cd9d2cbb67fcaa5d5
Corbin <cds@corbinsimpson.com>
2016-08-14 11:21:19 -0700

    afl: Fix.

--- afl.sh
+++ afl.sh
@@ -6,4 +6,4 @@ nix-env -i afl
 nix-build -A typhonDumpMAST
 [[ -d testcases ]] || mkdir testcases
 [[ -d broken ]] || mkdir broken
-afl-fuzz -i testcases -o broken result/mt-dump-mast @@
+afl-fuzz -i testcases -o broken result/bin/mt-dump-mast @@
