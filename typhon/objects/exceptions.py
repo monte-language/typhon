06db982efb8833d29360d34ccc97dd2775cc0dae
Allen Short <ashort@mozilla.com>
2017-05-30 23:20:32 +0000

    Make sealed exceptions Semitransparent

--- typhon/objects/exceptions.py
+++ typhon/objects/exceptions.py
@@ -1,12 +1,16 @@
 from typhon.atoms import getAtom
-from typhon.autohelp import autohelp
-from typhon.objects.auditors import deepFrozenStamp
+from typhon.autohelp import autohelp, method
+from typhon.errors import UserException
+from typhon.objects.auditors import (SealedPortrayal, deepFrozenStamp,
+                                     selfless, semitransparentStamp)
+from typhon.objects.collections.helpers import asSet
 from typhon.objects.collections.lists import wrapList
+from typhon.objects.collections.maps import EMPTY_MAP
 from typhon.objects.data import StrObject
 from typhon.objects.ejectors import throwStr
 from typhon.objects.root import Object, runnable
 
-
+RUN_1 = getAtom(u"run", 1)
 RUN_2 = getAtom(u"run", 2)
 
 
@@ -31,6 +35,26 @@ class SealedException(Object):
 
     def toString(self):
         return u"<sealed exception>"
+
+    def auditorStamps(self):
+        return asSet([selfless, semitransparentStamp])
+
+    @method("Any")
+    def _uncall(self):
+        return SealedPortrayal(wrapList([
+            makeSealedException, StrObject(u"run"),
+            wrapList([self.ue.getPayload()]), EMPTY_MAP]))
+
+
+@runnable(RUN_1, _stamps=[deepFrozenStamp])
+def _makeSealedException(e):
+    """
+    Seal an exception.
+    """
+    return SealedException(UserException(e))
+
+
+makeSealedException = _makeSealedException()
 
 
 @runnable(RUN_2, _stamps=[deepFrozenStamp])
