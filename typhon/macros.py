27d53023c8c97e89113c245ba88fb093adc70b7e
Allen Short <ashort@mozilla.com>
2018-04-16 20:04:37 +0000

    basic op/callback organization done

--- typhon/macros.py
+++ typhon/macros.py
@@ -1,12 +1,12 @@
 import __builtin__
 from ast import (Assign, Expr, Subscript, Attribute, List, Tuple, Name, Import,
-                 ImportFrom, FunctionDef, ClassDef, ListComp, For, Num,
-                 TryExcept, ExceptHandler, Call, Store, Load, alias, arguments,
-                 copy_location, fix_missing_locations)
+                 ImportFrom, FunctionDef, ClassDef, ListComp, For, Num, If, Eq,
+                 TryExcept, ExceptHandler, Call, Store, Load, Compare, alias,
+                 arguments, copy_location, fix_missing_locations, Str, Raise)
 from collections import namedtuple
 import copy
-from macropy.core.macros import Macros, Walker, injected_vars, post_processing
-from typhon.futures import OK
+from macropy.core.macros import (Macros, Walker, injected_vars,
+                                 post_processing)
 macros = Macros()
 
 
@@ -48,7 +48,8 @@ def rewriteFreeNames(tree, stop, **ctx):
     if type(tree) is Name:
         if (tree.id not in ctx['moduleGlobals'] and
                 tree.id not in __builtin__.__dict__):
-            if tree.id not in ctx['freeNames'] and tree.id not in ctx['boundNames']:
+            if (tree.id not in ctx['freeNames'] and
+                    tree.id not in ctx['boundNames']):
                 ctx['freeNames'].append(tree.id)
             stop()
             return Attribute(Name(ctx['selfName'], Load()), tree.id, tree.ctx)
@@ -67,118 +68,185 @@ def rewriteAsCallback(expr, state, globalNames, boundNames, freeNames):
     return expr
 
 
-JumpItem = namedtuple('JumpItem', ['exName', 'exStart', 'exEnd',
-                                   'elseStart', 'elseEnd'])
-FlowItem = namedtuple('FlowItem', ['opNum', 'expr', 'targetName'])
 Op = namedtuple('Op', ['expr', 'successIndex', 'failIndex',
                        'successName', 'failName'])
 
 
 def buildOperationsTable(lines):
     """
-    Flatten try/except statements to prepare them for generation of callbacks.
+    Dissects control and data flow to prepare for generating callbacks.
+    Returns a list of tuples, in reverse order of execution, containing:
+     * a single expression
+     * Relative address (or None) for the next operation on success
+     * Relative address (or None) for the next operation on failure
+     * Name to bind (or None) on success
+     * Name to bind (or None) on failure
     """
-    opCount = [0]
     flowList = []
 
-    def collectLine(line):
+    def collectLine(line, failTarget, failName, successTarget):
         if isinstance(line, Assign):
             assert isinstance(line.targets[0], Name), (
                 "assignment in io blocks must be to single names")
-            flowList.append(FlowItem(opCount[0], line.value,
-                                     line.targets[0].id))
-            opCount[0] += 1
+            flowList.append(Op(line.value, successTarget if flowList else None,
+                               failTarget, line.targets[0].id, failName))
         elif isinstance(line, Expr):
-            flowList.append(FlowItem(opCount[0], line.value, None))
-            opCount[0] += 1
+            flowList.append(Op(line.value, successTarget if flowList else None,
+                               failTarget, None, failName))
         elif isinstance(line, TryExcept):
-            tryIdx = len(flowList)
-            flowList.append("try")
-            for tryline in line.body:
-                collectLine(tryline)
             assert len(line.handlers) == 1, (
                 "try statements in io blocks must "
                 "have exactly one except block")
-            exName = line.handlers[0].name.id
-            exStart = opCount[0]
-            for excline in line.handlers[0].body:
-                collectLine(excline)
-            exEnd = opCount[0] - 1
-            elseStart = None
-            elseEnd = None
+            # jump target at end of else
+            exitPoint = len(flowList)
             if line.orelse:
-                elseStart = opCount[0]
-                for elseline in line.orelse:
-                    collectLine(elseline)
-                elseEnd = opCount[0] - 1
-            flowList[tryIdx] = JumpItem(exName, exStart, exEnd,
-                                        elseStart, elseEnd)
-
+                # process else block in reverse order
+                collectLine(line.orelse[-1], failTarget, failName,
+                            successTarget)
+                for elseline in reversed(line.orelse[:-1]):
+                    collectLine(elseline, failTarget, failName, -1)
+            # jump target at beginning of else
+            elsePoint = len(flowList)
+            exName = line.handlers[0].name.id
+            excSuccessTarget = (exitPoint - len(flowList) + successTarget
+                                if exitPoint else None)
+            excFailTarget = (exitPoint - len(flowList) + failTarget
+                             if failTarget else None)
+            # process except block in reverse order
+            collectLine(line.handlers[0].body[-1], excFailTarget, failName,
+                        excSuccessTarget)
+            for excline in reversed(line.handlers[0].body[:-1]):
+                excFailTarget = (exitPoint - len(flowList) + failTarget
+                                 if failTarget else None)
+                collectLine(excline, excFailTarget, failName, -1)
+            #jump target at beginning of catch block
+            catchPoint = len(flowList)
+            if line.orelse:
+                # jump into else when done
+                trySuccessTarget = elsePoint - len(flowList) - 1
+            else:
+                # jump to end of except when done
+                trySuccessTarget = (exitPoint - len(flowList) + successTarget
+                                    if exitPoint else None)
+            # process try block in reverse order
+            collectLine(line.body[-1], -1, exName, trySuccessTarget)
+            for tryline in reversed(line.body[:-1]):
+                collectLine(tryline, catchPoint - len(flowList) - 1,
+                            exName, -1)
         else:
             raise SyntaxError("Expected assign statement, call, or try/except "
                               "statement in io block, not " +
                               line.__class__.__name__)
 
-    for line in lines:
-        collectLine(line)
-    opList = []
-    jumpStack = []
-    finalIndex = flowList[-1].opNum
-    for item in flowList[:-1]:
-        if isinstance(item, JumpItem):
-            jumpStack.append(item)
+    for line in reversed(lines):
+        collectLine(line, None, None, -1)
+    return flowList
+
+
+CallbackInfo = namedtuple(
+    'CallbackInfo', ['base', 'successName', 'successExpr', 'successIndex',
+                     'failName', 'failExpr', 'failIndex'])
+
+
+def opsToCallbacks(originalOpList):
+    """
+    Convert a list of ops into callbacks.
+
+    Returns a dict of names bound to initial constants and a list, in execution
+    order, containing tuples of:
+     * Base class expression for callback class
+     * Name to bind (or None) for success value received
+     * Expression to execute upon success
+     * Callback number to pass control to on success
+     * Name to bind (or None) for error value received
+     * Expression to execute upon failure
+     * Callback number to pass control to on error
+    """
+    ops = []
+    initialState = {}
+    # collect initial constants and remove from ops list
+    while True:
+        oo = originalOpList[-1]
+        if isinstance(oo.expr, (Num, Str)) and oo.successName:
+            initialState[oo.successName] = oo.expr
+            del originalOpList[-1]
         else:
-            opNum, expr, successName = item
-            while (jumpStack and
-                   opNum >= (jumpStack[-1].elseEnd or
-                             jumpStack[-1].exEnd)):
-                jumpStack.pop()
-            if not jumpStack:
-                # No questions about control flow.
-                opList.append(Op(expr, opNum + 1, None, successName, None))
-                continue
-            for j in reversed(jumpStack):
-                if opNum < j.exStart:
-                    failIndex = j.exStart
-                    failName = j.exName
-                    break
-            else:
-                failIndex = None
-                failName = None
-            # assume we're continuing to next line
-            successIndex = opNum + 1
-            for j in reversed(jumpStack):
-                # are we at the end of a try block? continue to else block
-                if successIndex == j.exStart:
-                    if j.elseStart:
-                        successIndex = j.elseStart
-                    else:
-                        # No else block for this try block.
-                        # Gets tricky here, we might have further instructions
-                        # or we might be on a control edge still
-                        successIndex = j.exEnd + 1
-                        continue
-                # are we at the end of an except block?
-                # continue after else block
-                elif j.elseStart is not None:
-                    if successIndex == j.elseStart:
-                        # we've wandered out of an except block
-                        successIndex = j.elseEnd + 1
-                        continue
-                elif opNum == j.exEnd:
-                    successIndex = (j.elseEnd or j.exEnd) + 1
-                    continue
-                break
-            if successIndex > finalIndex:
-                successIndex = None
-            opList.append(Op(expr, successIndex, failIndex, successName,
-                             failName))
-    opList.append(Op(flowList[-1].expr, None, None, None, None))
-    return opList
+            break
+
+    # Convert relative addresses to absolute addresses
+
+    ops = [op._replace(successIndex=i - op.successIndex
+                       if op.successIndex is not None else None,
+                       failIndex=i - op.failIndex
+                       if op.failIndex is not None else None)
+           for i, op in enumerate(reversed(originalOpList))]
+    successNeedsPatching = {}
+    failNeedsPatching = {}
+    callbacks = []
+    for expr, successIndex, failIndex, successName, failName in ops:
+        successExpr = None
+        failExpr = None
+        if successIndex:
+            # Look at successor op and put its expr in this callback
+            successExpr = ops[successIndex].expr
+            if ops[successIndex].successIndex or ops[successIndex].failIndex:
+                # Fix up this callback later with address of callback following
+                # this expr (if any)
+                successNeedsPatching.setdefault(successExpr, set()).add(
+                    len(callbacks))
+        if failIndex:
+            failExpr = ops[failIndex].expr
+            if ops[failIndex].successIndex or ops[failIndex].failIndex:
+                failNeedsPatching.setdefault(failExpr, set()).add(
+                    len(callbacks))
+        if successExpr or failExpr:
+            callbacks.append(CallbackInfo(
+                Attribute(expr.func, "callbackType", Load()),
+                successName, successExpr, None,
+                failName, failExpr, None))
+
+        successPatchTargets = successNeedsPatching.get(expr, ())
+        for i in successPatchTargets:
+            callbacks[i] = callbacks[i]._replace(
+                successIndex=len(callbacks) - 1)
+        failPatchTargets = failNeedsPatching.get(expr, ())
+        for i in failPatchTargets:
+            callbacks[i] = callbacks[i]._replace(
+                failIndex=len(callbacks) - 1)
+    return initialState, callbacks
 
 
 @macros.block
 def io(tree, target, gen_sym, moduleGlobals, toEmit, **kw):
+    """
+    RPython doesn't allow many things that are convenient in Python, such as
+    nested functions, and furthermore its type checker is very restrictive.
+    This makes callback-based async IO rather verbose to implement.  To cope
+    with this, Typhon uses MacroPy to provide a small sublanguage for chaining
+    IO actions together.  (If you're familiar with Haskell, it's similar in
+    spirit to to 'do' notation.)
+
+    The basic idea is that inside a block started by `with io:`, execution does
+    not proceed in a straight line as it would with normal Python code.
+    Instead, each statement is turned into a callback that gets invoked when
+    the operation from the previous statement is complete.  The only statements
+    allowed in an `io` block are:
+      * Try/except/else (with a single except block)
+      * literals and calls invoking IO actions
+      * Assignment to a single name
+
+    IO blocks effectively have their own scope.  Assigning to names within them
+    will not modify names defined outside them.  Typically they are used by
+    creating a Monte promise to represent the final result of the sequence of
+    IO actions, and invoking the associated resolver when the IO actions are
+    complete.
+
+    The use of try/except is similarly restricted; `except` blocks are
+    unconditionally invoked when an action in the `try` block signals failure,
+    rather than matching on exception type.  Therefore the idiom is to use
+    `except object as err:` since the macro implementation ignores the type
+    normally used for matching.
+    """
     firstCallback = nextCallback = gen_sym("iostart_callback")
     stateClassName = gen_sym("State")
     FutureCtx_ = gen_sym("FutureCtx")
@@ -186,62 +254,29 @@ def io(tree, target, gen_sym, moduleGlobals, toEmit, **kw):
     Err_ = gen_sym("Err")
     freeNames = []
     boundNames = {}
-    targetName = None
     topLine = None
     None_ = Name("None", Load())
     nextCallbackBase = Name("object", Load())
+    successSym = None
+    failSym = None
     toEmit.append(ImportFrom("typhon.futures", [alias("FutureCtx", FutureCtx_),
                                                 alias("Ok", OK_),
                                                 alias("Err", Err_)],
                              0))
-    for i, line in enumerate(tree):
-        def emit(node):
-            toEmit.append(copy_location(node, line))
-        assert isinstance(line, (Assign, Expr, TryExcept)), "Only assignments and calls allowed in io block"
-        expr = line.value
-        if isinstance(expr, Num) and isinstance(line, Assign):
-            boundNames[line.targets[0].id] = expr
-            continue
-        assert isinstance(expr, Call), "io block line must be a call to an io operation"
+    emittedCallbacks = [None]
+    failureTargets = [None]
+    def emitCallbackClass(callbackBody):
         self_ = gen_sym("self")
         state = gen_sym("state")
         result = gen_sym("result")
-        if targetName:
-            status = gen_sym("status")
-            err = gen_sym("err")
-            collectResult = [Assign([Tuple([Name(status, Store()),
-                                            Name(targetName, Store()),
-                                            Name(err, Store())], Store())],
-                                    Name(result, Load())),
-                             Assign([Attribute(Name(state, Load()),
-                                               targetName, Store())],
-                                    Name(targetName, Load()))]
-            if targetName not in boundNames:
-                boundNames[targetName] = None_
-        else:
-            collectResult = []
-        opname = expr.func
-        opnameStr = opname.id if isinstance(opname, Name) else None
-        callbackName = nextCallback
         callbackClassName = callbackName + "_Class"
-        callbackBase = nextCallbackBase
-        if (i + 1) == len(tree):
-            nextCallback = "None"
-        else:
-            nextCallback = gen_sym(opnameStr or "io")
-            nextCallbackBase = Attribute(expr.func, "callbackType", Load()) #if  else Name("object", Load())
-
-        callbackBody = Expr(Call(Attribute(
-            rewriteAsCallback(copy.deepcopy(expr), state, moduleGlobals,
-                              boundNames, freeNames), "run", Load()),
-                            [Name(state, Load()), Name(nextCallback, Load())], [], None, None))
         callbackMethod = FunctionDef("do",
                                      arguments(
                                          [Name(self_, Store()),
                                           Name(state, Store()),
                                           Name(result, Store())],
                                          None, None, []),
-                                     collectResult + [callbackBody],
+                                     callbackBody,
                                      [])
         callbackClass = ClassDef(callbackClassName,
                                  [callbackBase],
@@ -250,6 +285,48 @@ def io(tree, target, gen_sym, moduleGlobals, toEmit, **kw):
         callbackInstance = Assign([Name(callbackName, Store())],
                                   Call(Name(callbackClassName, Load()), [], [], None, None))
         emit(callbackInstance)
+
+    def emit(line):
+        emittedCallbacks.append(generate(line, emittedCallbacks[-1],
+                                         failureTargets[-1]))
+    def generate(line, successExpr, failExpr):
+        expr = line.value
+        successSym = gen_sym("value")
+        failSym = gen_sym("err")
+        if successCallback or failCallback:
+            status = gen_sym("status")
+            collectResult = [Assign([Tuple([Name(status, Store()),
+                                            Name(successSym, Store()),
+                                            Name(failSym, Store())], Store())],
+                                    Name(result, Load())),]
+                             # Assign([Attribute(Name(state, Load()),
+                             #                   successSym, Store())],
+                             #        Name(successName, Load()))]
+        else:
+            collectResult = []
+
+        callbackBody = Expr(Call(Attribute(
+            rewriteAsCallback(copy.deepcopy(expr), state, moduleGlobals,
+                              boundNames, freeNames), "run", Load()),
+                            [Name(state, Load()), Name(successCallback, Load())], [], None, None))
+        
+
+        if isinstance(expr, Num) and successName:
+            boundNames[successName] = expr
+            continue
+        assert isinstance(expr, Call), "io block line must be a call to an io operation"
+        opname = expr.func
+        opnameStr = opname.id if isinstance(opname, Name) else None
+        callbackName = nextCallback
+        callbackBase = nextCallbackBase
+        if (i + 1) == len(tree):
+            nextCallback = "None"
+        else:
+            successSym = gen_sym(successName or "value")
+            errSym = gen_sym(failName or "err")
+            nextCallback = gen_sym(opnameStr or "io")
+            nextCallbackBase = Attribute(expr.func, "callbackType", Load()) #if  else Name("object", Load())
+
         if isinstance(line, Assign):
             # XXX could support tuple assignment here if we wanted
             assert isinstance(line.targets[0], Name), ("assignment in io blocks"
@@ -258,6 +335,9 @@ def io(tree, target, gen_sym, moduleGlobals, toEmit, **kw):
 
         else:
             targetName = None
+    for line in reversed(tree):
+        emit(line)
+        
     createState = Call(Name(stateClassName, Load()),
                        [Name(n, Load()) for n in freeNames], [], None, None)
     topLine = copy_location(Expr(
--- typhon/test/test_macros.py
+++ typhon/test/test_macros.py
@@ -1,7 +1,7 @@
 import ast
 from unittest import TestCase
 from macropy.core import unparse
-from typhon.macros import buildOperationsTable
+from typhon.macros import buildOperationsTable, opsToCallbacks
 
 sampleAst1 = """
 f = 0
@@ -71,22 +71,17 @@ class TestIOBlock(TestCase):
         self.assertEqual(
             [[unparse(line[0]).strip()] + list(line[1:]) for line in output],
             [
-                ["0",
-                 1, None, "f", None],
-                ["ruv.magic_fsOpen(vat, path, os.O_RDONLY, 0)",
-                 3, 2, "f", "err"],
-                ['smash(r, StrObject((u"Couldn\'t open file fount: %s" % err)))',
-                 None, None, None, None],
-                ["readLoop(f, buf)",
-                 6, 4, "contents", "err"],
-                ["ruv.magic_fsClose(vat, f)",
-                 5, None, None, None],
+                ["resolve(r, BytesObject(contents))", None, None, None, None],
+                ["ruv.magic_fsClose(vat, f)", -1, None, None, None],
                 ["smash(r, StrObject((u'libuv error: %s' % err)))",
                  None, None, None, None],
-                ["ruv.magic_fsClose(vat, f)",
-                 7, None, None, None],
-                ["resolve(r, BytesObject(contents))",
-                 None, None, None, None]
+                ["ruv.magic_fsClose(vat, f)", -1, None, None, None],
+                ["readLoop(f, buf)", -3, -1, "contents", "err"],
+                ['smash(r, StrObject((u"Couldn\'t open file fount: %s" % err)))',
+                 None, None, None, None],
+                ['ruv.magic_fsOpen(vat, path, os.O_RDONLY, 0)', -2, -1, "f",
+                "err"],
+                ['0', -1, None, 'f', None]
             ])
 
         input = ast.parse(sampleAst2).body
@@ -94,32 +89,19 @@ class TestIOBlock(TestCase):
         self.assertEqual(
             [[unparse(line[0]).strip()] + list(line[1:]) for line in output],
             [
-                ["1",
-                 1, None, "x", None],
-                ["io1()",
-                 2, 3, "x", "err1"],
-                ["io2()",
-                 6, 3, "y", "err1"],
-                ["io3()",
-                 4, None, None, None],
-                ["io4()",
-                 12, 5, None, "err2"],
-                ["io5()",
-                 12, None, None, None],
-                ["io6()",
-                 7, None, None, None],
-                ["io7()",
-                 8, 9, None, "err3"],
-                ["io8()",
-                 10, 9, None, "err3"],
-                ["io9()",
-                 11, None, None, None],
-                ["ioA()",
-                 11, None, None, None],
-                ["ioB()",
-                 12, None, None, None],
-                ["ioC()",
-                 None, None, None, None]
+                ["ioC()", None, None, None, None],
+                ["ioB()", -1, None, None, None],
+                ["ioA()", -1, None, None, None],
+                ["io9()", -2, None, None, None],
+                ["io8()", -2, -1, None, "err3"],
+                ["io7()", -1, -2, None, "err3"],
+                ["io6()", -1, None, None, None],
+                ["io5()", -7, None, None, None],
+                ["io4()", -8, -1, None, "err2"],
+                ["io3()", -1, None, None, None],
+                ["io2()", -4, -1, "y", "err1"],
+                ["io1()", -1, -2, "x", "err1"],
+                ["1", -1, None, "x", None]
             ])
 
         input = ast.parse(sampleAst3).body
@@ -127,17 +109,68 @@ class TestIOBlock(TestCase):
         self.assertEqual(
             [[unparse(line[0]).strip()] + list(line[1:]) for line in output],
             [
-                ["io0()",
-                 1, 5, None, "err2"],
-                ["io1()",
-                 2, 3, "x", "err1"],
-                ["io2()",
-                 4, 3, "y", "err1"],
-                ["io3()",
-                 6, 5, None, "err2"],
-                ["io4()",
-                 6, 5, None, "err2"],
-                ["io5()",
-                 6, None, None, None],
-                ["io6()",
-                 None, None, None, None]])
+                ["io6()", None, None, None, None],
+                ["io5()", -1, None, None, None],
+                ["io4()", -2, -1, None, "err2"],
+                ["io3()", -3, -2, None, "err2"],
+                ["io2()", -2, -1, "y", "err1"],
+                ["io1()", -1, -2, "x", "err1"],
+                ["io0()", -1, -5, None, "err2"]
+            ])
+
+    def testOpsToCallbacks(self
+    ):
+        """
+        Op lists are converted to callback lists properly.
+        """
+        def flattenOps(ops):
+            return  [[unparse(line[0]).strip(), line[1],
+                      line[2] and unparse(line[2]).strip(),
+                      line[3], line[4], line[5] and unparse(line[5]).strip(),
+                      line[6]] for line in ops]
+        input = ast.parse(sampleAst1).body
+        initialState, output = opsToCallbacks(buildOperationsTable(input))
+        self.assertEqual(initialState.keys(), ['f'])
+        self.assertEqual([unparse(v) for v in initialState.values()], ['0'])
+        self.assertEqual(
+            flattenOps(output),
+            [
+                ['ruv.magic_fsOpen.callbackType', "f", "readLoop(f, buf)", 1, "err", 'smash(r, StrObject((u"Couldn\'t open file fount: %s" % err)))', None],
+                ['readLoop.callbackType', "contents", "ruv.magic_fsClose(vat, f)", 3, "err", "ruv.magic_fsClose(vat, f)", 2],
+                ['ruv.magic_fsClose.callbackType', None, "smash(r, StrObject((u'libuv error: %s' % err)))", None, None, None, None],
+                ['ruv.magic_fsClose.callbackType', None, "resolve(r, BytesObject(contents))", None, None, None, None]
+                ])
+
+        input = ast.parse(sampleAst2).body
+        initialState, output = opsToCallbacks(buildOperationsTable(input))
+        self.assertEqual(initialState.keys(), ['x'])
+        self.assertEqual([unparse(v) for v in initialState.values()], ['1'])
+        self.assertEqual(
+            flattenOps(output),
+            [
+                ['io1.callbackType', 'x', 'io2()', 1, 'err1', 'io3()', 2],
+                ['io2.callbackType', 'y', 'io6()', 5, 'err1', 'io3()', 2],
+                ['io3.callbackType', None, 'io4()', 3, None, None, None],
+                ['io4.callbackType', None, 'ioC()', None, 'err2', 'io5()', 4],
+                ['io5.callbackType', None, 'ioC()', None, None, None, None],
+                ['io6.callbackType', None, 'io7()', 6, None, None, None],
+                ['io7.callbackType', None, 'io8()', 7, 'err3', 'io9()', 8],
+                ['io8.callbackType', None, 'ioA()', 9, 'err3', 'io9()', 8],
+                ['io9.callbackType', None, 'ioB()', 10, None, None, None],
+                ['ioA.callbackType', None, 'ioB()', 10, None, None, None],
+                ['ioB.callbackType', None, 'ioC()', None, None, None, None]
+            ])
+
+        input = ast.parse(sampleAst3).body
+        initialState, output = opsToCallbacks(buildOperationsTable(input))
+        self.assertEqual(initialState, {})
+        self.assertEqual(
+            flattenOps(output),
+            [
+                ['io0.callbackType', None, 'io1()', 1, 'err2', 'io5()', 5],
+                ['io1.callbackType', 'x', 'io2()', 2, 'err1', 'io3()', 3],
+                ['io2.callbackType', 'y', 'io4()', 4, 'err1', 'io3()', 3],
+                ['io3.callbackType', None, 'io6()', None, 'err2', 'io5()', 5],
+                ['io4.callbackType', None, 'io6()', None, 'err2', 'io5()', 5],
+                ['io5.callbackType', None, 'io6()', None, None, None, None],
+            ])
