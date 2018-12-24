9c2aae5d53379506a08fa3548a0844d624085c8d
Corbin <cds@corbinsimpson.com>
2018-12-23 20:42:38 -0800

    nix-support: Use Nix, not Make, to build modules.
    This provides us with incremental per-module compilation.

--- nix-support/mast.nix
+++ nix-support/mast.nix
@@ -1,43 +1,68 @@
-{stdenv, pkgs, lib, typhonVm, mastSrc}:
-
-stdenv.mkDerivation {
-    name = "typhon";
-
-    src = mastSrc;
-
-    buildInputs = [ typhonVm
-      # Needed for lit.sh
-      pkgs.gawk pkgs.less ];
-
-    shellHook = ''
-    function rrTest() {
-       CNT=0
-       ln -s ${typhonVm}/mt-typhon .
-       while make testMast MT_TYPHON="${pkgs.rr}/bin/rr record ${typhonVm}/mt-typhon"; do
-           make clean
-           let CNT++
-           echo $CNT
-       done
-       echo $CNT
-    }
+{stdenv, pkgs, lib, typhonVm}:
+let
+  boot = ../boot;
+  mastSrc = ../mast;
+  buildMonteModule = name: mt: let
+    basename = baseNameOf mt;
+    flags = if basename == "prelude.mt" || basename == "loader.mt"
+      then "-noverify"
+      else "";
+  in pkgs.runCommand name {} ''
+      ${typhonVm}/mt-typhon -l ${boot} ${boot}/loader run montec ${flags} ${mt} $out
     '';
-
-    # Make lit.sh call bash directly instead of invoking an inner nix-shell;
-    # with Nix 2 the inner-shell trick doesn't work right.
-    patchPhase = ''
-      sed -i -e '1 s|^.*$|#!${pkgs.bash}/bin/bash|' lit.sh
-    '';
-
-    buildPhase = ''
-      ln -s ${typhonVm}/mt-typhon .
-      make
-    '';
-
-    installPhase = ''
-      mkdir -p $out/bin
-      cp -r mast loader.mast $out/
-    '';
-
-    checkPhase = "make testMast";
-    doCheck = true;
-}
+  mtToMast = filename: (lib.removeSuffix ".mt" filename) + ".mast";
+  buildMonteTree = root: context: files: lib.concatLists (lib.mapAttrsToList (filename: filetype:
+    let
+      rel = if context == "" then filename else context + ("/" + filename);
+      abs = root + ("/" + rel);
+    in if filetype == "directory"
+      then buildMonteTree root rel (builtins.readDir abs)
+      else if lib.hasSuffix ".mt" filename then [{
+        name = mtToMast rel;
+        path = buildMonteModule (mtToMast filename) abs;
+      }] else []
+  ) files);
+  tree = buildMonteTree mastSrc "" (builtins.readDir mastSrc);
+  buildMASTFarm = pkgs.linkFarm "mast" tree;
+in buildMASTFarm
+  # in stdenv.mkDerivation {
+  #     name = "typhon";
+  # 
+  #     src = mastSrc;
+  # 
+  #     buildInputs = [ typhonVm
+  #       # Needed for lit.sh
+  #       pkgs.gawk pkgs.less ];
+  # 
+  #     shellHook = ''
+  #     function rrTest() {
+  #        CNT=0
+  #        ln -s ${typhonVm}/mt-typhon .
+  #        while make testMast MT_TYPHON="${pkgs.rr}/bin/rr record ${typhonVm}/mt-typhon"; do
+  #            make clean
+  #            let CNT++
+  #            echo $CNT
+  #        done
+  #        echo $CNT
+  #     }
+  #     '';
+  # 
+  #     # Make lit.sh call bash directly instead of invoking an inner nix-shell;
+  #     # with Nix 2 the inner-shell trick doesn't work right.
+  #     patchPhase = ''
+  #       sed -i -e '1 s|^.*$|#!${pkgs.bash}/bin/bash|' lit.sh
+  #     '';
+  # 
+  #     buildPhase = ''
+  #       ln -s ${typhonVm}/mt-typhon .
+  #       make
+  #     '';
+  # 
+  #     installPhase = ''
+  #       mkdir -p $out/bin
+  #       cp -r mast loader.mast $out/
+  #     '';
+  # 
+  #     checkPhase = "make testMast";
+  #     doCheck = true;
+  # }
--- nix-support/typhon.nix
+++ nix-support/typhon.nix
@@ -10,20 +10,6 @@ let
          (type == "directory" || lib.hasSuffix ".py" p)) ||
       p == loc "/typhon" ||
       p == loc "/main.py") ./..;
-  mastSrc = let loc = part: (toString ./..) + part;
-     in builtins.filterSource (path: type:
-      let p = toString path;
-       in ((lib.hasPrefix (loc "/mast/") p &&
-            (type == "directory" || lib.hasSuffix ".mt" p || lib.hasSuffix ".mt.md" p)) ||
-           (lib.hasPrefix (loc "/boot/") p &&
-            (type == "directory" || lib.hasSuffix ".ty" p || lib.hasSuffix ".mast" p)) ||
-        p == loc "/mast" ||
-        p == loc "/boot" ||
-        p == loc "/Makefile" ||
-        p == loc "/lit.sh" ||
-        p == loc "/loader.mast" ||
-        p == loc "/lit.sh" ||
-        p == loc "/repl.mast")) ./..;
   pypy = nixpkgs.pypy.override { packageOverrides = (s: su: {
     mock = su.mock.overridePythonAttrs (old: { doCheck = false; });
     pytest = su.pytest.overridePythonAttrs (old: { doCheck = false; });
@@ -39,8 +25,7 @@ let
   typhon = with nixpkgs; rec {
     typhonVm = callPackage ./vm.nix (vmConfig // { buildJIT = false; });
     typhonVmJIT = callPackage ./vm.nix (vmConfig // { buildJIT = true; });
-    mast = callPackage ./mast.nix { mastSrc = mastSrc;
-                                    typhonVm = typhonVmJIT;
+    mast = callPackage ./mast.nix { typhonVm = typhonVmJIT;
                                     pkgs = nixpkgs; };
 
     typhonDumpMAST = callPackage ./dump.nix {};
